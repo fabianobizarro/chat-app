faa9846208ba5fce7d86d37d64724bdd1ce61ab8
Fabiano Bizarro <fabianobizarro@outlook.com>
2017-03-17 06:48:51 -0300

    animated.css added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './style/app.css';
 import '../node_modules/bulma/bulma.sass'
+import '../node_modules/animate.css/animate.min.css'
 import App from './app/App';
 
 ReactDOM.render(
